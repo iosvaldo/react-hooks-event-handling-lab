1a6f9b4f4589b455c47e189bb5b3cd361dc59b01
osvaldo <iosvaldo.app@gmail.com>
2022-05-21 20:01:27 -0400

    commit tree afb9596df9976008d68ec22d67c411dd56d18a8c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function eventHandler(){
+  // e.preventDefult()
+  console.log("Good!")
+}
+
+function EyesOnMe() {
+  return (
+    <div>
+      <button onFocus={eventHandler} onBlur={()=> console.log("Hey! Eyes on me!")}>"Eyes on me"</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
